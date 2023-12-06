5a0399c86147c98f90f5945ac189ebf14100fcfc
rbxo3213 <rbxo3213@gmail.com>
2023-12-06 21:33:55 +0900

    first commit

---
+++ pokemon.sql
@@ -0,0 +1,51 @@
+CREATE TABLE TypeMatchup (
+    Type VARCHAR(10) PRIMARY KEY,
+    Normal FLOAT,
+    Fire FLOAT,
+    Water FLOAT,
+    Electric FLOAT,
+    Grass FLOAT,
+    Ice FLOAT,
+    Fighting FLOAT,
+    Poison FLOAT,
+    Ground FLOAT,
+    Flying FLOAT,
+    Psychic FLOAT,
+    Bug FLOAT,
+    Rock FLOAT,
+    Ghost FLOAT,
+    Dragon FLOAT,
+    Dark FLOAT,
+    Steel FLOAT,
+    Fairy FLOAT
+);
+
+INSERT INTO TypeAdvantages (TypeName, Normal, Fire, Water, Electric, Grass, Ice, Fighting, Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy)
+VALUES
+('Normal', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1),
+('Fire', 1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1),
+('Water', 1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1),
+('Electric', 1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1),
+('Grass', 1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1),
+('Ice', 1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1),
+('Fighting', 2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5),
+('Poison', 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2),
+('Ground', 1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1),
+('Flying', 1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1),
+('Psychic', 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1),
+('Bug', 1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5),
+('Rock', 1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1),
+('Ghost', 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1),
+('Dragon', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0),
+('Dark', 1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5),
+('Steel', 1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2),
+('Fairy', 1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1);
+
+CREATE TABLE Skills (
+    SkillID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    Type VARCHAR(50),
+    Power INT,
+    Accuracy INT,
+    Category VARCHAR(50)
+);
